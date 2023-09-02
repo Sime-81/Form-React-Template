d59b7cebbf893922f0bd19492af3190feae9f821
Yapock <yapock123@gmail.com>
2023-09-02 23:54:22 +0200

    Form-template envoix de mail

---
+++ src/FormTemplate.js
@@ -0,0 +1,57 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const FormTemplate = () => {
+  const form = useRef();
+  const formMess = document.querySelector(".form-message");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ngo8r3l",
+        "template_h38ihuc",
+        form.current,
+        process.env.REACT_APP_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = "<p class='success'>Email envoyer !</p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        },
+        (error) => {
+          console.log(error.text);
+
+          formMess.innerHTML =
+            "<p class='error'>Une erreur est survenue, veuillez réasséyer !</p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        }
+      );
+  };
+
+  return (
+    <div className="form-container">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" required autoComplete="off" />
+        <label>Email</label>
+        <input type="email" name="email" required autoComplete="off" />
+        <label>Message</label>
+        <textarea name="message" required />
+        <input type="submit" value="Envoyer" />
+      </form>
+      <div className="form-message"></div>
+    </div>
+  );
+};
+
+export default FormTemplate;
